dc77c1f3fd90219aeaaec928d6f70242c2e29b22
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-09-02 19:29:03 -0500

    Ejercicio Bisección de Bolzano completo.

---
+++ Lab1/BiseccionBolzano.m
@@ -0,0 +1,12 @@
+function c = BiseccionBolzano(f,a,b,delta)
+    while abs(b - a) >= delta
+        c = a + (b-a)/2;
+        if f(c) == 0
+            break;
+        elseif sign(f(a)) ~= sign(f(c))
+            b = c;
+        elseif sign(f(c)) ~= sign(f(b))
+            a = c;
+        end
+    end
+end
---
+++ Lab1/CalcBiseccionBolzano.m
@@ -0,0 +1,18 @@
+func = input('Ingresa una función en terminos de x: ','s');
+f = inline(func,'x');
+delta = input('Ingresa un valor de tolerancia delta: ');
+while 1   
+    a = input('Ingrese un límite inferior del intervalo: ');
+    b = input('Ingrese un límite superior del intervalo: ');
+    if sign(f(a)) == sign(f(b));
+        disp('Los intervalos no son validos, por favor ingreselos nuevamente.');
+    else
+        break
+    end
+end
+format short
+root = BiseccionBolzano(f, a, b, delta);
+result = ['La aproximación de la raíz de la función' ' ' func ' en el intervalo [' num2str(a) ', '  num2str(b)  '] es:' ' ' num2str(root)];
+disp(result)
+%disp(strcat('La aproximación de la raíz de la función',{' '} ,func ,' en el intervalo [', num2str(a),', ', num2str(b) ,'] es:',{' '}, num2str(root)));
+%disp('La aproximación de la raíz de la función' + func + ' en el intervalo [' + num2str(a) + ', ' + num2str(b) + '] es:' + num2str(root));
