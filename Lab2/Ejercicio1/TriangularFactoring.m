5236ed25e35d1f2db4702b1591774614cd41460e
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-09-16 12:19:58 -0500

    Arreglo en función de Factorización triangular.

--- Lab2/Ejercicio1/TriangularFactoring.m
+++ Lab2/Ejercicio1/TriangularFactoring.m
@@ -1,14 +1,26 @@
 function matrix = TriangularFactoring(M, B)
     matrix = [M B];
 
-    for a = 1 : size(matrix, 1) - 1;
+    for a = 1 : size(matrix, 1);
+        row = a + 1;
+        while matrix(a, a) == 0;
+            if row <= size(matrix, 1);
+                for b = 1 : size(matrix, 2)
+                    temp = matrix(a, b);
+                    matrix(a, b) = matrix(row, b);
+                    matrix(row, b) = temp;    
+                end
+            else
+               error("\nLa matriz ingresada es singular.\n");
+            end
+        end
 
         for b = a + 1 : size(matrix, 1);
             matrix(b, a) = matrix(b, a) / matrix(a, a);
         end
         for b = a + 1 : size(matrix, 1);
             for c = a + 1 : size(matrix, 2);
-            matrix(b, c) = matrix(b, c) - matrix(b,a) * matrix(a, c);
+                matrix(b, c) = matrix(b, c) - matrix(b, a) * matrix(a, c);
             end
         end
 
