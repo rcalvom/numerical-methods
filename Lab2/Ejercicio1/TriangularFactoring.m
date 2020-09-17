eb04237ed52eb72aeeeb105b9fa738904a2e85d0
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-09-17 08:19:58 -0500

    Optimización en pivoteo de funcion TriangularFactoring.

--- Lab2/Ejercicio1/TriangularFactoring.m
+++ Lab2/Ejercicio1/TriangularFactoring.m
@@ -1,16 +1,22 @@
-function matrix = TriangularFactoring(M, B)
-    matrix = [M B];
+function matrix = TriangularFactoring(M)
+    matrix = M;
     for a = 1 : size(matrix, 1);
-        row = a + 1;
-        while matrix(a, a) == 0;
-            if row <= size(matrix, 1);
-                for b = 1 : size(matrix, 2)
+        if matrix(a, a) == 0;
+            row = 0;
+            for b = a + 1 : size(matrix, 1);
+                if matrix(b, a) ~= 0;
+                    row = b;
+                    break;
+                end
+            end
+            if row == 0;
+                error("\nLa matriz ingresada es singular, no se puede factorizar.\n");
+            else
+                for b = 1 : size(matrix, 2);
                     temp = matrix(a, b);
                     matrix(a, b) = matrix(row, b);
                     matrix(row, b) = temp;    
                 end
-            else
-                error("\nLa matriz ingresada es singular, no se puede factorizar.\n");
             end
         end
         for b = a + 1 : size(matrix, 1);
