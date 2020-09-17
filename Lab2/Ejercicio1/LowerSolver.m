1198b4b25b78a44f561cc0def7430cea7a85a246
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-09-16 22:27:19 -0500

    Corrección menor en LowerSolver.

--- Lab2/Ejercicio1/LowerSolver.m
+++ Lab2/Ejercicio1/LowerSolver.m
@@ -4,6 +4,6 @@ function Y = LowerSolver(L, B)
         for b = 1 : a - 1;
             acc = acc + L(a, b) * Y(b, 1);
         end
-        Y(a, 1) = (B(a, 1) - acc) / L(a, a);
+        Y(a, 1) = (B(a, 1) - acc);
     end    
 end
