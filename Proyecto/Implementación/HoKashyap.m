6b323016d0a877a1daffdddb17afabb5f1b4735f
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-12-15 18:48:41 -0500

    Proyecto final terminado

--- Proyecto/Implementación/HoKashyap.m
+++
@@ -1,5 +0,0 @@
-function [a, b] = HosKashyap(a, b, Y, eta)
-    e = Y*a - b;
-    b = b + eta * (e + abs(e));
-    a = (Y' * Y)^-1 * Y' * b;
-end
--- Proyecto/Implementación/vector2sym.m
+++
@@ -1,5 +0,0 @@
-function y = vector2sym(vector)
-    syms x;
-    sympref('FloatingPointOutput', true)
-    y = - vector(2) / vector(3) * x - vector(1) / vector(3);
-end
